d21ab1bf2dbd6b3068ba2dd22fbf7d7069fc4b93
Seth Bowman <brandonsethbowman@gmail.com>
2023-08-17 08:30:26 -0500

    practice completed

---
+++ DapperRefresh/DepartmentRepo.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperRefresh
+{
+    public class DepartmentRepo : IDepartmentRepo
+    {
+        private readonly IDbConnection _connection;
+
+        public DepartmentRepo(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;");
+        }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO Departments (Name) VALUES (@departmentName);",
+                new { departmentName = newDepartmentName});
+        }
+    }
+}
--- DapperRefresh/Program.cs
+++ DapperRefresh/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using DapperRefresh;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var repo = new DepartmentRepo(conn);
+
+
+var departmens = repo.GetAllDepartments();
+
+foreach(var dep in departmens)
+{
+    Console.WriteLine(dep.Name);
+}
